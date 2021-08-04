413683b2db277f8ec72ed413da41af2d38110c35
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-04 21:04:20 +0300

    add Global styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 import Header from './Header';
 import Footer from './Footer';
 
+const theme = {
+  black: '#262626',
+  yellow: '#ffc600',
+  lightgrey: '#efefef',
+  grey: '#3a3a3a',
+};
+
 const Layout = ({ children }) => (
-  <div>
-    <Header />
-    <div>{children}</div>
-    <Footer />
-  </div>
+  <ThemeProvider theme={theme}>
+    <StyledPage>
+      <GlobalStyles />
+      <Header />
+      <Content>{children}</Content>
+      <Footer />
+    </StyledPage>
+  </ThemeProvider>
 );
 
 export default Layout;
+
+const StyledPage = styled.div`
+  background: #ffffff;
+  color: ${props => props.theme.black};
+`;
+
+const Content = styled.div`
+  max-width: 900px;
+  min-heigth: 40vw;
+  padding: 6rem 1 rem;
+  margin: 0 auto;
+`;
