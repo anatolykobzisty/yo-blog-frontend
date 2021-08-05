445f2faf42eacac01b7a26f960773cdd970f69b2
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-05 18:38:07 +0300

    add Footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import FooterStyles from '../styles/FooterStyles';
 
 const Footer = () => (
-  <div>
-    <div>&copy; {new Date().getFullYear()}</div>
-  </div>
+  <FooterStyles>
+    <div className="wrapperForm">
+      <legend>
+        <span className="formAction">Подписаться</span> на нашу рассылку
+      </legend>
+    </div>
+    <div className="wrapperMain">
+      <Link to="/">Fresh Blog </Link>
+      &copy; {new Date().getFullYear()}
+    </div>
+  </FooterStyles>
 );
 
 export default Footer;
