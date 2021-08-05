49ed31ea5fd88f77c7babb765ff04a300f507e3f
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-05 18:37:52 +0300

    add Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import { SiHellofresh } from 'react-icons/si';
 import { Link } from 'gatsby';
 
 const Header = () => (
-  <ul>
-    <li>
-      <Link to="/">Главная</Link>
-    </li>
-    <li>
-      <Link to="/postone">Статья №1</Link>
-    </li>
-  </ul>
+  <Container>
+    <Wrapper>
+      <Link to="/">
+        <h1>
+          <SiHellofresh /> Fresh Blog
+        </h1>
+      </Link>
+    </Wrapper>
+  </Container>
 );
+
+const Container = styled.header`
+  background: #ffffff;
+  box-shadow: rgba(0, 0, 0, 0.06) 0px 4px 4px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  h1 {
+    font-size: 28px;
+    font-family: 'Balsamiq Sans', cursive;
+    font-weight: 700;
+    margin: 0;
+    display: flex;
+    align-items: center;
+  }
+
+  a {
+    color: ${props => props.theme.black};
+    text-decoration: none;
+    svg {
+      fill: ${props => props.theme.yellow};
+      width: 40px;
+      height: 40px;
+    }
+  }
+`;
 
 export default Header;
