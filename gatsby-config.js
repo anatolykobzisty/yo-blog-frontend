cca3a63491e7dbc8f05ddb89130baea87130de74
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-06 17:38:42 +0300

    setting gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `Yo Blog`,
+    siteUrl: `https://yo-blog.ru`,
+    description: `Мой супер быстрый блог`,
+    author: 'Anatoly',
+    vk: `https://vk.com/yo-blog.ru`,
+  },
+};
