a5af49793fc8f2134ae6c891d3632a184c6e51d9
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-08 22:30:39 +0300

    add pwa config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,25 @@ module.exports = {
         token: process.env.SANITY_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        workboxConfig: {
+          globPatterns: ['*.html'],
+        },
+      },
+    },
   ],
 };
